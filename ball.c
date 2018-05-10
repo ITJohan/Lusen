63ebc4797ce673c90ab60217daaba238c64090c2
Johan Lindkvist <johanlindkvist89@gmail.com>
2018-05-10 20:11:19 +0200

    Update ball.c

--- ball.c
+++ ball.c
@@ -1,5 +1,14 @@
 #include "ball.h"
 
+unsigned short lfsr = 0xACE1u;
+unsigned short bit;
+
+unsigned short rand()
+{
+	bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
+	return lfsr =  (lfsr >> 1) | (bit << 15);
+}
+
 void set_object_speed(POBJECT o, int speedx, int speedy) {
 	o->dirx = speedx;
 	o->diry = speedy;
@@ -17,6 +26,20 @@ void clear_object(POBJECT o) {
 	}
 }
 
+void random_position(POBJECT o) {
+	short rand_x = ((rand() / 2) % 126) + 1;
+	short rand_y = ((rand() / 2) % 62) + 1;
+	for (int i = 0; i < 4; i++) {
+		o->geo->px[i].x = rand_x + i % 2;
+		if (i < 2)
+			o->geo->px[i].y = rand_y;
+		else
+			o->geo->px[i].y = rand_y + 1;
+	}
+	o->posx = rand_x;
+	o->posy = rand_y;
+}
+
 void update_object(POBJECT o) {
 	for(int i = 0; i < o->geo->numpoints; i++) {
 		o->geo->px[i].x += o->dirx;
@@ -30,14 +53,22 @@ void move_object(POBJECT o) {
 	clear_object(o);
 	update_object(o);
 	
-	if(o->posx < 1)
-		o->dirx *= -1;
-	if((o->posx + o->geo->sizex) > 128)
-		o->dirx *= -1;
-	if(o->posy < 1)
-		o->diry *= -1;
-	if((o->posy + o->geo->sizey) > 64)
-		o->diry *= -1;
+	if(o->posx < 1) {
+		draw_ascii("DEATH");
+		while(1){};
+	}
+	if((o->posx + o->geo->sizex) > 128) {
+		draw_ascii("DEATH");
+		while(1){};
+	}
+	if(o->posy < 1) {
+		draw_ascii("DEATH");
+		while(1){};
+	}
+	if((o->posy + o->geo->sizey) > 64) {
+		draw_ascii("DEATH");
+		while(1){};
+	}
 		
 	draw_object(o);
 }
